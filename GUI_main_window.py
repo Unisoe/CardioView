595af02fd93d0dbba7cc45733971817c74c4b032
Unisoe <114939931+Unisoe@users.noreply.github.com>
2023-03-24 11:26:13 -0400

    fixed layout, divided columns into two groups

--- GUI_main_window.py
+++ GUI_main_window.py
@@ -1,17 +1,15 @@
-import os
-import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMessageBox, QDesktopWidget
+from PyQt5.QtWidgets import QMessageBox, QGridLayout
 from patient_data_sqlite import new_entry, get_m_file
 from popup import NewUserDialog
 import RunProcessing
 import serial
 import struct
 
-
 class UiMainWindow(object):
     def __init__(self):
+        super().__init__()
         self.movie = None
         self.send_to_mc = None
         self.new_user = None
@@ -28,17 +26,17 @@ class UiMainWindow(object):
         self.disp_patient = None
         self.disp_speed = None
         self.file_search = None
-        self.gridLayout = None
-        self.line = None
-        self.line_2 = None
+        self.outerLayout = None
+        self.line_get = None
+        self.line_new = None
         self.date_txt = None
-        self.pat_name1 = None
+        self.pat_name_get = None
         self.pat_num_txt1 = None
         self.thresh_txt = None
         self.load_data_txt = None
         self.new_pat_data_title = None
         self.get_pat_data_title = None
-        self.pat_name2 = None
+        self.new_pat_name_label = None
         self.pat_num2 = None
         self.load_data = None
         self.menubar = None
@@ -59,233 +57,194 @@ class UiMainWindow(object):
         MainWindow.setObjectName("MainWindow")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
+        self.outerLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.outerLayout.setObjectName("outerLayout")
 
         stylesheet = "QWidget { font-size: 15px; }"
         self.centralwidget.setStyleSheet(stylesheet)
-
-
+        self.leftLayout = QGridLayout()
+        self.rightLayout = QGridLayout()
 
         self.ser_pat_name = QtWidgets.QLineEdit(self.centralwidget)
         self.ser_pat_name.setObjectName("ser_pat_name")
-        self.gridLayout.addWidget(self.ser_pat_name, 10, 5, 1, 4)
+        self.rightLayout.addWidget(self.ser_pat_name, 8, 3, 1, 3)
 
         self.ser_pat_num = QtWidgets.QLineEdit(self.centralwidget)
         self.ser_pat_num.setObjectName("ser_pat_num")
-        self.gridLayout.addWidget(self.ser_pat_num, 11, 5, 1, 4)
+        self.rightLayout.addWidget(self.ser_pat_num, 9, 3, 1, 3)
 
         self.thresh = QtWidgets.QLineEdit(self.centralwidget)
         self.thresh.setObjectName("thresh")
-        self.gridLayout.addWidget(self.thresh, 12, 5, 1, 1)
-
-        self.get_pat_data_title = QtWidgets.QLineEdit(self.centralwidget)
-        font = QtGui.QFont()
-        font.setBold(True)
-        font.setWeight(75)
-        self.get_pat_data_title.setFont(font)
-        self.get_pat_data_title.setAlignment(QtCore.Qt.AlignCenter)
-        self.get_pat_data_title.setReadOnly(True)
-        self.get_pat_data_title.setObjectName("lineEdit_7")
-        self.gridLayout.addWidget(self.get_pat_data_title, 9, 3, 1, 6)
-
-        self.thresh_txt = QtWidgets.QLineEdit(self.centralwidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.thresh_txt.sizePolicy().hasHeightForWidth())
-        self.thresh_txt.setSizePolicy(sizePolicy)
-        self.thresh_txt.setReadOnly(True)
-        self.thresh_txt.setObjectName("lineEdit_4")
-        self.gridLayout.addWidget(self.thresh_txt, 12, 3, 1, 1)
+        self.rightLayout.addWidget(self.thresh, 10, 3, 1, 1)
 
         # View Data Button
         self.view_data = QtWidgets.QPushButton(self.centralwidget)
         self.view_data.setObjectName("view_data")
         self.view_data.clicked.connect(self.ser_pat_info)
-        self.gridLayout.addWidget(self.view_data, 12, 7, 1, 1)
+        self.rightLayout.addWidget(self.view_data, 10, 4, 1, 1)
 
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout.addItem(spacerItem, 16, 1, 1, 9)
+        # Connect to MC button
+        self.connect_mc = QtWidgets.QPushButton(self.centralwidget) #edithere this might be the only button for connection
+        self.connect_mc.setObjectName("connect_mc")
+        self.connect_mc.clicked.connect(self.connect_to_model)
+        self.rightLayout.addWidget(self.connect_mc, 10, 5, 1, 1)
 
-        self.line = QtWidgets.QFrame(self.centralwidget)
-        self.line.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
-        self.line.setObjectName("line")
-        self.gridLayout.addWidget(self.line, 8, 3, 1, 6)
+        self.get_pat_data_title = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.get_pat_data_title.setFont(font)
+        self.get_pat_data_title.setAlignment(QtCore.Qt.AlignCenter)
+        self.get_pat_data_title.setObjectName("Get Patient Data Title")
+        self.rightLayout.addWidget(self.get_pat_data_title, 7, 2, 1, 4)
+
+        self.thresh_txt = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.thresh_txt.sizePolicy().hasHeightForWidth())
+        self.thresh_txt.setSizePolicy(sizePolicy)
+        self.thresh_txt.setObjectName("Thresh Label")
+        self.rightLayout.addWidget(self.thresh_txt, 10, 2, 1, 1)
+
+        self.line_get = QtWidgets.QFrame(self.centralwidget)
+        self.line_get.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line_get.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line_get.setObjectName("line_get")
+        self.rightLayout.addWidget(self.line_get, 6, 2, 1, 4)
 
         self.disp_speed = QtWidgets.QSlider(self.centralwidget)
         self.disp_speed.setOrientation(QtCore.Qt.Horizontal)
-        self.disp_speed.setObjectName("disp_speed")
-        self.disp_speed.setRange(1, 10)
+        self.disp_speed.setObjectName("Disp Speed Slider")
+        self.disp_speed.setRange(0, 10)
         self.disp_speed.setValue(5)
         self.disp_speed.setEnabled(False)
         self.disp_speed.valueChanged.connect(self.slider_value_changed)
-        self.gridLayout.addWidget(self.disp_speed, 10, 1, 1, 1)
+        self.leftLayout.addWidget(self.disp_speed, 7, 1, 1, 1)
 
-        spacerItem1 = QtWidgets.QSpacerItem(17, 88, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem1, 10, 2, 4, 1)
-
-        self.pat_num_txt1 = QtWidgets.QLineEdit(self.centralwidget)
+        self.pat_num_txt1 = QtWidgets.QLabel(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pat_num_txt1.sizePolicy().hasHeightForWidth())
         self.pat_num_txt1.setSizePolicy(sizePolicy)
-        self.pat_num_txt1.setReadOnly(True)
-        self.pat_num_txt1.setObjectName("lineEdit_3")
-        self.gridLayout.addWidget(self.pat_num_txt1, 11, 3, 1, 1)
+        self.pat_num_txt1.setObjectName("New Pat Num Label")
+        self.rightLayout.addWidget(self.pat_num_txt1, 4, 2, 1, 1)
 
-        self.disp_patient = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.disp_patient = QtWidgets.QLabel(self.centralwidget)
         self.disp_patient.setObjectName("disp_patient")
-        self.disp_patient.setReadOnly(True)
-        self.gridLayout.addWidget(self.disp_patient, 11, 1, 3, 1)
+        self.leftLayout.addWidget(self.disp_patient, 8, 1, 3, 1)
 
-        self.pat_name1 = QtWidgets.QLineEdit(self.centralwidget)
+        self.pat_name_get = QtWidgets.QLabel(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pat_name1.sizePolicy().hasHeightForWidth())
-        self.pat_name1.setSizePolicy(sizePolicy)
-        self.pat_name1.setReadOnly(True)
-        self.pat_name1.setObjectName("lineEdit_2")
-        self.gridLayout.addWidget(self.pat_name1, 10, 3, 1, 1)
+        sizePolicy.setHeightForWidth(self.pat_name_get.sizePolicy().hasHeightForWidth())
+        self.pat_name_get.setSizePolicy(sizePolicy)
+        self.pat_name_get.setObjectName("Get Patient Label")
+        self.rightLayout.addWidget(self.pat_name_get, 8, 2, 1, 1)
 
         # self.upload_mc = QtWidgets.QPushButton(self.centralwidget) #edithere maybe remove this button, have connection/send same button
         # self.upload_mc.setObjectName("upload_mc")
-        # self.gridLayout.addWidget(self.upload_mc, 13, 8, 1, 1)
-
-        self.connect_mc = QtWidgets.QPushButton(self.centralwidget) #edithere this might be the only button for connection
-        self.connect_mc.setObjectName("connect_mc")
-        self.connect_mc.clicked.connect(self.connect_to_model)
-        self.gridLayout.addWidget(self.connect_mc, 12, 8, 1, 1)
-
-        spacerItem2 = QtWidgets.QSpacerItem(20, 127, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem2, 10, 4, 3, 1)
+        # self.outerLayout.addWidget(self.upload_mc, 13, 8, 1, 1)
 
         # upload to database button
         self.upload_database = QtWidgets.QPushButton(self.centralwidget)
         self.upload_database.setObjectName("upload_database")
         self.upload_database.clicked.connect(self.new_pat_info)
-        self.gridLayout.addWidget(self.upload_database, 6, 8, 1, 1) #edithere
+        self.rightLayout.addWidget(self.upload_database, 5, 5, 1, 1)  # edithere
 
-        spacerItem3 = QtWidgets.QSpacerItem(808, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.gridLayout.addItem(spacerItem3, 0, 0, 1, 10)
-
-        spacerItem4 = QtWidgets.QSpacerItem(20, 356, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem4, 1, 0, 13, 1)
-
-        spacerItem5 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem5, 1, 2, 9, 1)
-
-        self.new_pat_data_title = QtWidgets.QLineEdit(self.centralwidget)
+        self.new_pat_data_title = QtWidgets.QLabel(self.centralwidget)
         font = QtGui.QFont()
         font.setBold(True)
         font.setWeight(75)
         self.new_pat_data_title.setFont(font)
         self.new_pat_data_title.setAlignment(QtCore.Qt.AlignCenter)
-        self.new_pat_data_title.setReadOnly(True)
         self.new_pat_data_title.setObjectName("lineEdit_6")
-        self.gridLayout.addWidget(self.new_pat_data_title, 1, 3, 1, 6)
+        self.rightLayout.addWidget(self.new_pat_data_title, 1, 2, 1, 4)
 
-        self.line_2 = QtWidgets.QFrame(self.centralwidget)
-        self.line_2.setFrameShape(QtWidgets.QFrame.HLine)
-        self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
-        self.line_2.setObjectName("line_2")
-        self.gridLayout.addWidget(self.line_2, 2, 3, 1, 6)
-
-        self.load_data_txt = QtWidgets.QLineEdit(self.centralwidget)
+        self.load_data_txt = QtWidgets.QLabel(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.load_data_txt.sizePolicy().hasHeightForWidth())
         self.load_data_txt.setSizePolicy(sizePolicy)
-        self.load_data_txt.setReadOnly(True)
         self.load_data_txt.setObjectName("lineEdit_5")
-        self.gridLayout.addWidget(self.load_data_txt, 3, 3, 1, 1)
-
-        spacerItem6 = QtWidgets.QSpacerItem(20, 88, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem6, 3, 4, 4, 1)
+        self.rightLayout.addWidget(self.load_data_txt, 2, 2, 1, 1)
 
         self.load_data = QtWidgets.QLineEdit(self.centralwidget)
         self.load_data.setObjectName("load_data")
-        self.gridLayout.addWidget(self.load_data, 3, 5, 1, 3)
+        self.rightLayout.addWidget(self.load_data, 2, 3, 1, 2)
 
         # New user button
         self.new_user = QtWidgets.QPushButton(self.centralwidget) # edithere
         self.new_user.setObjectName("new_user")
         self.new_user.clicked.connect(self.new_user_window)
-        self.gridLayout.addWidget(self.new_user, 1, 1, 1, 1)
+        self.leftLayout.addWidget(self.new_user, 1, 1, 1, 1)
 
         # Create a figure and axis
         self.disp_graph = QtWidgets.QLabel(self.centralwidget)
-        self.disp_graph.setMinimumSize(QtCore.QSize(400, 400))
-        self.disp_graph.setBaseSize(QtCore.QSize(400, 400))
+        self.disp_graph.setMinimumSize(QtCore.QSize(200, 200))
+        self.disp_graph.setBaseSize(QtCore.QSize(200, 200))
         self.disp_graph.setObjectName("disp_graph")
-        self.gridLayout.addWidget(self.disp_graph, 2, 1, 8, 1)
+        self.leftLayout.addWidget(self.disp_graph, 2, 1, 5, 1)
 
-        spacerItem7 = QtWidgets.QSpacerItem(20, 356, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem7, 1, 9, 13, 1)
-
-        self.pat_name2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.new_pat_name_label = QtWidgets.QLabel(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pat_name2.sizePolicy().hasHeightForWidth())
-        self.pat_name2.setSizePolicy(sizePolicy)
-        self.pat_name2.setReadOnly(True)
-        self.pat_name2.setObjectName("lineEdit_8")
-        self.gridLayout.addWidget(self.pat_name2, 4, 3, 1, 1)
+        sizePolicy.setHeightForWidth(self.new_pat_name_label.sizePolicy().hasHeightForWidth())
+        self.new_pat_name_label.setSizePolicy(sizePolicy)
+        self.new_pat_name_label.setObjectName("lineEdit_8")
+        self.rightLayout.addWidget(self.new_pat_name_label, 3, 2, 1, 1)
 
         # new patient name
         self.new_pat_name = QtWidgets.QLineEdit(self.centralwidget)
         self.new_pat_name.setObjectName("new_pat_name")
-        self.gridLayout.addWidget(self.new_pat_name, 4, 5, 1, 4)
+        self.rightLayout.addWidget(self.new_pat_name, 3, 3, 1, 3)
 
-        self.pat_num2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.pat_num2 = QtWidgets.QLabel(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pat_num2.sizePolicy().hasHeightForWidth())
         self.pat_num2.setSizePolicy(sizePolicy)
-        self.pat_num2.setReadOnly(True)
         self.pat_num2.setObjectName("lineEdit_9")
-        self.gridLayout.addWidget(self.pat_num2, 5, 3, 1, 1)
+        self.rightLayout.addWidget(self.pat_num2, 9, 2, 1, 1)
 
         # new patient number
         self.new_pat_num = QtWidgets.QLineEdit(self.centralwidget)
         self.new_pat_num.setObjectName("new_pat_num")
-        self.gridLayout.addWidget(self.new_pat_num, 5, 5, 1, 4)
+        self.rightLayout.addWidget(self.new_pat_num, 4, 3, 1, 3)
 
-        self.date_txt = QtWidgets.QLineEdit(self.centralwidget)
+        self.date_txt = QtWidgets.QLabel(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.date_txt.sizePolicy().hasHeightForWidth())
         self.date_txt.setSizePolicy(sizePolicy)
-        self.date_txt.setReadOnly(True)
         self.date_txt.setObjectName("lineEdit_1")
-        self.gridLayout.addWidget(self.date_txt, 6, 3, 1, 1)
+        self.rightLayout.addWidget(self.date_txt, 5, 2, 1, 1)
 
         self.date = QtWidgets.QDateEdit(self.centralwidget)
         self.date.setObjectName("date")
-        self.gridLayout.addWidget(self.date, 6, 5, 1, 3) #edithere
+        self.rightLayout.addWidget(self.date, 5, 3, 1, 2) #edithere
 
         # File Search Button
         self.file_search = QtWidgets.QPushButton(self.centralwidget)
         self.file_search.setObjectName("file_search")
         self.file_search.clicked.connect(self.openFileNameDialog)
-        self.gridLayout.addWidget(self.file_search, 3, 8, 1, 1)
+        self.rightLayout.addWidget(self.file_search, 2, 5, 1, 1)
 
         # self.prog_database = QtWidgets.QProgressBar(self.centralwidget)
         # self.prog_database.setProperty("value", 24)
         # self.prog_database.setObjectName("prog_database")
-        # self.gridLayout.addWidget(self.prog_database, 7, 3, 1, 5)
+        # self.outerLayout.addWidget(self.prog_database, 7, 3, 1, 5)
         #
         # self.prog_mc = QtWidgets.QProgressBar(self.centralwidget)
         # self.prog_mc.setProperty("value", 24)
         # self.prog_mc.setObjectName("prog_mc")
-        # self.gridLayout.addWidget(self.prog_mc, 13, 3, 1, 5)
+        # self.outerLayout.addWidget(self.prog_mc, 13, 3, 1, 5)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -299,6 +258,10 @@ class UiMainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.outerLayout.addLayout(self.leftLayout)
+        self.outerLayout.addLayout(self.rightLayout)
+        self.setLayout(self.outerLayout)
 
     def openFileNameDialog(self):
         options = QtWidgets.QFileDialog.Options()
@@ -369,13 +332,13 @@ class UiMainWindow(object):
         self.thresh_txt.setText(_translate("MainWindow", "Threshold:"))
         self.view_data.setText(_translate("MainWindow", "View Data"))
         self.pat_num_txt1.setText(_translate("MainWindow", "Patient Number:"))
-        self.pat_name1.setText(_translate("MainWindow", "Patient Name:"))
+        self.pat_name_get.setText(_translate("MainWindow", "Patient Name:"))
         # self.upload_mc.setText(_translate("MainWindow", "Upload to Model"))
         self.connect_mc.setText(_translate("MainWindow", "Connect to Model"))
         self.upload_database.setText(_translate("MainWindow", "Upload to Database"))
         self.new_pat_data_title.setText(_translate("MainWindow", "NEW PATIENT DATA"))
         self.load_data_txt.setText(_translate("MainWindow", "Load Data:"))
-        self.pat_name2.setText(_translate("MainWindow", "Patient Name:"))
+        self.new_pat_name_label.setText(_translate("MainWindow", "Patient Name:"))
         self.pat_num2.setText(_translate("MainWindow", "Patient Number:"))
         self.date_txt.setText(_translate("MainWindow", "Date:"))
         self.file_search.setText(_translate("MainWindow", "File Search"))
--- testing_functions.py
+++ testing_functions.py
@@ -1,51 +1,39 @@
-# from PyQt5.QtCore import QSize
-#
-# class MainWindow(QMainWindow):
-#     def __init__(self):
-#         QMainWindow.__init__(self)
-#
-#         self.setMinimumSize(QSize(300, 200))
-#         self.setWindowTitle("PyQt messagebox example - pythonprogramminglanguage.com")
-#
-#         pybutton = QPushButton('Show messagebox', self)
-#         pybutton.clicked.connect(self.clickMethod)
-#         pybutton.resize(200,64)
-#         pybutton.move(50, 50)
-#
-#     def clickMethod(self):
-#         ret = QMessageBox.question(self, 'MessageBox', "Click a button",
-#                                    QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel, QMessageBox.Cancel)
-#
-#         if ret == QMessageBox.Yes:
-#             print('Button QMessageBox.Yes clicked.')
-#
-# if __name__ == "__main__":
-#     app = QtWidgets.QApplication(sys.argv)
-#     mainWin = MainWindow()
-#     mainWin.show()
-#     sys.exit( app.exec_() )
-
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QMovie, QPixmap
-from PyQt5.QtCore import Qt
 import sys
 
-from patient_data_sqlite import get_gif_path, get_m_file
+from PyQt5.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QFormLayout,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget, QGridLayout,
+)
 
-app = QApplication(sys.argv)
-window = QWidget()
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Nested Layouts Example")
+        # Create an outer layout
+        outerLayout = QVBoxLayout()
+        # Create a form layout for the label and line edit
+        topLayout = QFormLayout()
+        # Add a label and a line edit to the form layout
+        topLayout.addRow("Some Text:", QLineEdit())
+        # Create a layout for the checkboxes
+        optionsLayout = QGridLayout()
+        # Add some checkboxes to the layout
+        optionsLayout.addWidget(QCheckBox("Option one"))
+        optionsLayout.addWidget(QCheckBox("Option two"))
+        optionsLayout.addWidget(QCheckBox("Option three"))
+        # Nest the inner layouts into the outer layout
+        outerLayout.addLayout(topLayout)
+        outerLayout.addLayout(optionsLayout)
+        # Set the window's main layout
+        self.setLayout(outerLayout)
 
-m_file, gif_file, pat_name, pat_num, date = get_m_file('a',1)
-
-gif_label = QLabel(window)
-gif = QMovie(gif_file)
-gif_label.setMovie(gif)
-gif_size = QPixmap(gif_file).size()
-gif_label.resize(gif_size)
-gif.start()
-
-window.setWindowTitle("Display GIF")
-window.show()
-
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
 
